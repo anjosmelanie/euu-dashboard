e3ea1d40f6e162f6b56a6fc5ddce260b12775ee4
anjosmelanie <a.melanie993@gmail.com>
2025-04-12 14:43:55 +0200

    wip fixing styling and cleaning up constants

--- helpers/scripts/hcscript.js
+++ helpers/scripts/hcscript.js
@@ -1,5 +1,8 @@
-// Scripts for history/countries
+// Imports from other js files
+import { CULTURE_OPTIONS, TRIGGER_RELIGIONS, RELIGIOUS_SCHOOL_OPTIONS } from './constants.js';
+import { defaultOption } from './utils.js';
 
+// Constants for history/countries
 const nameInput = document.getElementById('name');
 const tagInput = document.getElementById('tag');
 const governmentSelect = document.getElementById('government');
@@ -69,7 +72,7 @@ governmentSelect.addEventListener('change', () => {
   const reforms = governmentReformOptions[selectedGovernment] || [];
 
   // Clear current template options
-  governmentReformSelect.innerHTML = '<option value="">-- Select a government reform --</option>';
+  governmentReformSelect.innerHTML = defaultOption('government reform');
 
   // Populate new ones
   reforms.forEach(reform => {
@@ -88,16 +91,16 @@ const religiousSchoolOptions = {
 
 religionSelect.addEventListener('change', () => {
     const selectedReligion = religionSelect.value;
-    const schools = religiousSchoolOptions[selectedReligion] || [];
+    const schools = RELIGIOUS_SCHOOL_OPTIONS[selectedReligion] || [];
 
-    if (triggerReligions.includes(selectedReligion)) {
+    if (TRIGGER_RELIGIONS.includes(selectedReligion)) {
         religiousSchoolWrapper.style.display = 'block';
     } else {
         religiousSchoolWrapper.style.display = 'none';
     }
 
     // Clear current template options
-    religiousSchoolSelect.innerHTML = '<option value="">-- Select a government reform --</option>';
+    religiousSchoolSelect.innerHTML = defaultOption('religious school');
 
     // Populate new ones
     schools.forEach(school => {
@@ -109,87 +112,12 @@ religionSelect.addEventListener('change', () => {
   });
 
 // Culture Group dropdown
-  const primaryCultureOptions = {
-    "Aboriginal": ["Gamilaraay", "Gunwinyguan", "Kulin", "Kuric", "Nyoongah", "Palawa", "Paman", "Yura"],
-    "Altaic": ["Kazakh", "Khalkha", "Korchin", "Kyrgyz", "Mongol", "Oirat", "Turkmeni", "Uyghur", "Uzbek"],
-    "Andean": ["Aimara", "Chimuan", "Diaguita", "Quechua"],
-    "Apachean": ["Eastern Apache", "Lipan", "Mescalero", "Navajo"],
-    "Araucanian": ["Het", "Huarpe", "Mapuche", "Patagonian"],
-    "Aridoamerican": ["Chichimecan", "Guamares", "Otomi", "Tecos", "Tepic", "Yaqui"],
-    "Baltic": ["Estonian", "Latvian", "Lithuanian"],
-    "British": ["American", "Cornish", "English", "Scottish", "Welsh"],
-    "Burman": ["Arakanese", "Burmese", "Chin", "Kachin", "Karen"],
-    "Byzantine": ["Cappadocian", "Gothic", "Greco-Georgian", "Greek", "Griko", "Pontic"],
-    "Caddoan": ["Caddo", "Pawnee", "Wichita"],
-    "Caribbean": ["Carib", "Lokono", "Maipurean", "Taino"],
-    "Carpathian": ["Hungarian", "Romanian", "Transylvanian"],
-    "Caucasian": ["Armenian", "Circassian", "Dagestani", "Georgian"],
-    "Celtic": ["Highlander", "Irish"],
-    "Central Algonquian": ["Anicinabe", "Anishinabe", "Cree", "Illini", "Innu-Naskapi", "Mesquakie", "Myaamia", "Shawnee"],
-    "Central American": ["Aztec", "Matlantzinca", "Purepecha", "Totonac"],
-    "Central Indian": ["Garjati", "Gondi", "Jharkandi"],
-    "Chibchan": ["Caran", "Miskito", "Muisca"],
-    "Chinese": ["Cantonese", "Gan", "Hakka", "Hubei", "Jianghuai", "Jin", "Manchu", "Min", "Shandong", "Sichuanese", "Sino-Altaic", "Sino-Korean", "Sino-Tibetan", "Sino-Vietnamese", "Sino-Zhuang", "Wu", "Xiang", "Xibei", "Zhili", "Zhongyuan"],
-    "Cushitic": ["Afar", "Amhara", "Harari", "Oromo", "Sidamo", "Somali", "Tigray"],
-    "Dravidian": ["Kannada", "Malayalam", "Tamil", "Telugu"],
-    "East Bantu": ["Bena", "Malagasy", "Swahili", "Takama", "Thagiicu"],
-    "Eastern Algonquian": ["Abenaki", "Delaware", "Mahican", "Maliseet", "Massachusset", "Mikmaq", "Pequot", "Powhatan"],
-    "Eastern Aryan": ["Assamese", "Bengali", "Bihari", "Kochi", "Nepali", "Oriya", "Pahari", "Sinhalese"],
-    "East Slavic": ["Byelorussian", "Muscovite", "Novgorodian", "Ruthenian", "Ryazanian"],
-    "Eskaleut": ["Aleutian", "Inuit"],
-    "Evenki": ["Buryat", "Evenk", "Jurchen", "Tungus", "Yakut", "Yukaghir"],
-    "French": ["Anglois", "Aquitanian", "Breton", "Burgundian", "Francien", "Gascon", "Louisianan", "Norman", "Occitan", "Québécois", "Walloon"],
-    "Germanic": ["Austrian", "Balten", "Bavarian", "Dutch", "Flemish", "Franconian", "Frisian", "Gothic", "Lower Saxon", "Pomeranian", "Prussian", "Rhenish", "Saxon", "Swabian", "Swiss", "Westphalian"],
-    "Great Lakes": ["Ganda", "Masaba", "Rwandan"],
-    "Hindustani": ["Avadhi", "Hindavi", "Kashmiri", "Panjabi", "Vindhyan"],
-    "Iberian": ["Andalusian", "Aragonese", "Basque", "Brazilian", "Castilian", "Catalan", "Galician", "Leonese", "Mexican", "Platinean", "Portuguese"],
-    "Iranian": ["Afghan", "Azerbaijani", "Baluchi", "Khorasani", "Kurdish", "Luri", "Mazandarani", "Persian", "Turco-Iranian"],
-    "Iroquoian": ["Cherokee", "Huron", "Iroquois", "Laurentian", "Susquehannock", "Tionontate"],
-    "Japanese": ["Kyushuan", "Saigoku", "Togoku"],
-    "Je": ["Charruan", "Ge"],
-    "Kamchatkan": ["Ainu", "Kamchatkan", "Nivkh"],
-    "Kongo": ["Chokwe", "Jukun", "Kongolese", "Kuba", "Luba", "Lunda", "Mbangala", "Mbundu", "Sawabantu", "Yaka"],
-    "Korean": ["Korean"],
-    "Latin": ["Dalmatian", "Ligurian", "Lombard", "Maltese", "Neapolitan", "Piedmontese", "Romagnol", "Sardinian", "Sicilian", "Tuscan", "Umbrian", "Venetian"],
-    "Levantine": ["Bedouin", "Egyptian", "Hejazi", "Khaleeji", "Mahri", "Mashriqi", "Omani", "Syrian", "Turkish", "Yemeni"],
-    "Lost Cultures": ["Anglo-Saxon", "Aramaic", "Athenian", "Atlantean", "Babylonian", "Etrurian", "Hebrew", "Jan Mayenese", "Livonian", "Old Egyptian", "Parthian", "Phoenician", "Pruthenian", "Roman", "Scanian", "Scythian", "Spartan"],
-    "Maghrebi": ["Algerian", "Berber", "Moroccan", "Tunisian"],
-    "Malay": ["Acehnese", "Bornean", "Cham", "Filipino", "Javanese", "Madagascan", "Malayan", "Moluccan", "Nusa Tenggara", "Sulawesi", "Sumatran", "Sundanese"],
-    "Mande": ["Bambara", "Bozo", "Dyola", "Mali", "Songhai", "Soninke"],
-    "Marañón": ["Chachapoyan", "Jivaro"],
-    "Matacoan": ["Chacoan"],
-    "Mayan": ["Chontales", "Highland Mayan", "Lacandon", "Mayan", "Putun", "Wastek", "Yucatec"],
-    "Mon-Khmer": ["Khmer", "Mon", "Vietnamese"],
-    "Muskogean": ["Catawba", "Chickasaw", "Choctaw", "Creek", "Natchez", "Yamasee", "Yoron", "Yuchi"],
-    "Na-Déné": ["Athabascan", "Chipewyan", "Haida"],
-    "Nordic": ["Danish", "Finnish", "Icelandic", "Karelian", "Norse", "Norwegian", "Sámi", "Swedish"],
-    "Otomanguean": ["Mixtec", "Tlapanec", "Zapotek"],
-    "Pacific": ["Maori", "Melanesian", "Papuan", "Polynesian"],
-    "Penutian": ["Chinook", "Salish", "Yokuts"],
-    "Plains Algonquian": ["Arapaho", "Blackfoot", "Bungi", "Cheyenne", "Plains Cree"],
-    "Sahelian": ["Bilala", "Fulani", "Hausa", "Kanuri", "Senegambian", "Tuareg", "Tunjur"],
-    "Siouan": ["Chiwere", "Dakota", "Nakota", "Osage"],
-    "Slavic": ["Bosnian", "Bulgarian", "Byelorussian", "Croatian", "Czech", "Muscovite", "Novgorodian", "Polish", "Ruthenian", "Ryazanian", "Serbian", "Silesian", "Slovak", "Slovene", "Sorbian"],
-    "Sonoran": ["Kiowa", "Piman", "Pueblo", "Shoshone"],
-    "Southern African": ["Bemba", "Khoisan", "Makua", "Nguni", "Nyasa", "Shona"],
-    "South Slavic": ["Albanian", "Bosnian", "Bulgarian", "Croatian", "Serbian", "Slovene"],
-    "Sudanese": ["Acholi", "Beja", "Nubian"],
-    "Tai": ["Khon Muang", "Lao", "Shan", "Siamese", "Zhuang"],
-    "Tatar": ["Astrakhani", "Bashkir", "Crimean", "Kazani", "Mishar", "Nogai", "Siberian"],
-    "Tibetan": ["Bai", "Miao", "Tibetan", "Yi"],
-    "Tupi": ["Guarani", "Tupinamba"],
-    "Ugric": ["Ingrian", "Ostyak", "Samoyed", "Uralic"],
-    "West African": ["Akan", "Dagomba", "Fon", "Mossi", "Nupe", "Yoruba"],
-    "Western Aryan": ["Gujarati", "Malvi", "Marathi", "Parsi", "Rajasthani", "Saurashtri", "Sindhi"],
-    "West Slavic": ["Czech", "Polish", "Silesian", "Slovak", "Sorbian"]
-  };
-
   cultureGroupSelect.addEventListener('change', () => {
     const selectedCultureGroup = cultureGroupSelect.value;
-    const cultures = primaryCultureOptions[selectedCultureGroup] || [];
+    const cultures = CULTURE_OPTIONS[selectedCultureGroup] || [];
   
     // Clear current template options
-    primaryCultureSelect.innerHTML = '<option value="">-- Select a primary culture --</option>';
+    primaryCultureSelect.innerHTML = defaultOption('primary culture');
   
     // Populate new ones
     cultures.forEach(culture => {
---
+++ helpers/scripts/utils.js
@@ -0,0 +1,3 @@
+export function defaultOption(label) {
+    return `<option value="">-- Select a ${label} --</option>`;
+  }
